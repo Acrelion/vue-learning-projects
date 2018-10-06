fce8afd34a269345383fd9ce3f7996854f2c08b6
Acrelion <iachi1763@gmail.com>
2018-10-06 20:26:35 +0300

    project-one-hello-vue completed

---
+++ project-one-hello-vue/main.js
@@ -0,0 +1,20 @@
+new Vue({
+    //the element to be manipulated
+    el: '#app',
+    //the data we need use to populate the element
+    data: {
+        title: 'Hello Vue!'
+    },
+    //
+    methods: {
+        changeTitle: function(event) {
+            /**
+             * all data properties are accessible by this as Vue
+             * proxies them to the upper most scope of the Vue object
+             * -bind the title to the event target value, which is input
+             * -input is an event called for each key stroke
+             */
+            this.title = event.target.value;
+        }
+    }
+});
